fe87f868a7d22dcfd933fcfedce7bca1e3750f76
lzkamods <bohmohhek677@outlook.com>
2023-11-08 23:33:36 +0200

    Russian snd switch languages Added

--- BeamSkinMaster/Views/LocalizationView.xaml.cs
+++ BeamSkinMaster/Views/LocalizationView.xaml.cs
@@ -17,28 +17,80 @@ using Microsoft.UI;
 using CommunityToolkit.Mvvm.DependencyInjection;
 using System.Reflection;
 using BeamSkinMaster.classes;
+using BeamSkinMaster.Models;
+using Microsoft.Windows.ApplicationModel.Resources;
+using System.Collections.ObjectModel;
+using Windows.Globalization;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace BeamSkinMaster.pages.Views
 {
+
+    public class LanguageItem
+    {
+        public LanguageItem(string languageTag, string displayName)
+        {
+            LanguageTag = languageTag;
+            DisplayName = displayName;
+        }
+
+        public string LanguageTag { get; }
+
+        public string DisplayName { get; }
+    }
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class LocalizationViev : Page
     {
+        private ResourceManager resourceManager = new();
+
+        private ResourceLoader resourceLoader = new();
+
         public LocalizatoinViewViewModel ViewModel { get; }
 
         public LocalizationViev()
         {
             this.InitializeComponent();
             ViewModel = Ioc.Default.GetRequiredService<LocalizatoinViewViewModel>();
+
+            LanguageItems.Add(new LanguageItem("en-US", this.resourceLoader.GetString("en-US")));
+            LanguageItems.Add(new LanguageItem("ru-RU", this.resourceLoader.GetString("ru-RU")));
         }
 
-        private void languagecombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
+        public ObservableCollection<LanguageItem> LanguageItems { get; } = new();
 
+        private void ComboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is not ComboBox languageTagComboBox)
+            {
+                return;
+            }
+
+            if (LanguageItems
+                .Where(x => x.LanguageTag == ApplicationLanguages.PrimaryLanguageOverride)
+                .FirstOrDefault() is LanguageItem currentLanguageItem)
+            {
+
+                languageTagComboBox.SelectedValue = currentLanguageItem;
+            }
+        }
+
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is not ComboBox languageTagComboBox ||
+            languageTagComboBox.SelectedValue is not LanguageItem selectedLanguageItem)
+            {
+                return;
+            }
+
+            ApplicationLanguages.PrimaryLanguageOverride = selectedLanguageItem.LanguageTag;
+
+            ResourceContext resourceContext = this.resourceManager.CreateResourceContext();
+            resourceContext.QualifierValues["Language"] = selectedLanguageItem.LanguageTag;
         }
     }
 }
